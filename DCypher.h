0a6efef8cc31b5231b03bc690dea1f65354a03e6
Damir Kazeev <dr.kazeev@yandex.ru>
2019-12-28 18:21:27 +0300

    add sets of true & false expresions

--- DCypher.h
+++ DCypher.h
@@ -5,9 +5,33 @@
 #include <iostream>
 #include <time.h>
 #include <algorithm>
+#include <set>
 
 class DCypher
 {
+    //переменная, значение которой мы не знаем
+    int private_key;
+    //константа, которую мы хотим зашифровать
+    int cypher_const;
+    //множество с всегда истинными функциями
+    std::set<std::string> true_expr {"(j * j > 0)",
+                                     "(i * i > 0)"
+                                     "(i * j == j * i"
+                                     "(i + j == j + i)"
+                                     "((i * j) * (i * j) == i * i + 2 * i * j + j * j)"
+                                     "(i + i * 2 == 3 * i)"
+                                     "(j + j == 2 * j)"};
+    //множество с всегда истинными функциями
+    std::set<std::string> false_expr {"(j * j <= 0)",
+                                     "(i * i <= 0)"
+                                     "(i * j != j * i"
+                                     "(i + j != j + i)"
+                                     "((i * j) * (i * j) != i * i + 2 * i * j + j * j)"
+                                     "(i + i * 2 != 3 * i)"
+                                     "(j + j != 2 * j)"};
+public:
+    //переменная, значение которой мы знаем
+    int public_key;
 
 };
 
