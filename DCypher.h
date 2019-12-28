e5526d7795e0d9c2d37d91f94eef1ae5a8a9f5e7
Damir Kazeev <dr.kazeev@yandex.ru>
2019-12-28 18:03:43 +0300

    Initial commit

---
+++ DCypher.h
@@ -0,0 +1,16 @@
+#ifndef DCYPHER_DCYPHER_H
+#define DCYPHER_DCYPHER_H
+
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <algorithm>
+
+class DCypher
+{
+
+};
+
+
+
+#endif //DCYPHER_DCYPHER_H
